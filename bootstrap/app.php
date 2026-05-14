80dc3cef46a158138918183b3684161ba2c0ab14
NICTruongbcn <hongtruongbvn2k5@gmail.com>
2026-05-14 22:59:45 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,15 +4,25 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Http\Middleware\CheckAuth;
+use App\Http\Middleware\roleMiddleware;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        $middleware->alias([
+            'auth.check' => CheckAuth::class,
+            'role' => roleMiddleware::class,
+        ]);
+
     })
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
